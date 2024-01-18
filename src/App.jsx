31eb9f8fa11f5a49a8f421919ca96a12ab6e1590
Shreyans T <shreyans.tatiya@gmail.com>
2024-01-19 01:56:52 +0530

    Updated some css.

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Navbar from '../components/navbar.jsx'
 import Hero from '../components/hero.jsx'
+import Card from '../components/card.jsx';
 
 export default function App() {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Card />
     </div>
   );
 }
